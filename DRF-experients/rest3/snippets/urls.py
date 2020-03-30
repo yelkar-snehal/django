6895b8f665eee5d870963b58a660fff626b11546
yelkar-snehal <yelkarsnehal9@gmail.com>
2020-03-30 22:51:45 +0530

    Add practice DRF application

---
+++ DRF-experients/rest3/snippets/urls.py
@@ -0,0 +1,15 @@
+from django.urls import path
+from rest_framework.urlpatterns import format_suffix_patterns
+from snippets import views
+
+urlpatterns = [
+    # function based views
+    # path('snippets/', views.snippet_list),
+    # path('snippets/<int:pk>/', views.snippet_detail),
+
+    # class based views
+    path('snippets/', views.SnippetList.as_view()),
+    path('snippets/<int:pk>/', views.SnippetDetail.as_view()),
+]
+
+urlpatterns = format_suffix_patterns(urlpatterns)

1824532cb239752751ee67fb3ab1c741c797c56d
yelkar-snehal <31803022+yelkar-snehal@users.noreply.github.com>
2020-04-25 21:20:12 +0530

    Feature/auth (#10)
    * Delete pycache and migrations
    
    * Delete db file
    
    * Delete migrations
    
    * Delete db file
    
    * Add auth feature

--- DRF-experiments/rest3/rest3/urls.py
+++ DRF-experiments/rest3/rest3/urls.py
@@ -15,8 +15,14 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf.urls import include
 
 urlpatterns = [
     path('', include('snippets.urls')),
     path('admin/', admin.site.urls),
+]
+
+# auth
+urlpatterns += [
+    path('api-auth/', include('rest_framework.urls')),
 ]

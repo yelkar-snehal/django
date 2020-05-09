8e79a907c9069bce9d9a6e97480f6e745da0e6bc
yelkar-snehal <yelkarsnehal9@gmail.com>
2020-05-09 12:33:55 +0530

    Register views

--- DRF-experiments/rest3/snippets/urls.py
+++ DRF-experiments/rest3/snippets/urls.py
@@ -64,3 +64,14 @@ user_list = UserViewSet.as_view({
 user_detail = UserViewSet.as_view({
     'get': 'retrieve'
 })
+
+
+# register the views with URL conf
+urlpatterns = format_suffix_patterns([
+    path('', api_root),
+    path('snippets/', snippet_list, name='snippet-list'),
+    path('snippets/<int:pk>/', snippet_detail, name='snippet-detail'),
+    path('snippets/<int:pk>/highlight/', snippet_highlight, name='snippet-highlight'),
+    path('users/', user_list, name='user-list'),
+    path('users/<int:pk>/', user_detail, name='user-detail')
+])
